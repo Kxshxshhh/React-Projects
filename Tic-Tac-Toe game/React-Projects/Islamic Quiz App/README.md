a8ba2c4312d27a8db9ff0abeee0f92c3a115d2b1
Kashish <141477985+Kxshxshhh@users.noreply.github.com>
2024-09-10 15:22:53 +0530

    Added TicTacToe Game project
    Have fun exploring through our childhood favourite Tic Tac Toe game 
    Made with React.js, in Vite bundler 
    Has the reset option to start a new game
    Displays the winner of the game

---
+++ Tic-Tac-Toe game/React-Projects/Islamic Quiz App/README.md
@@ -0,0 +1,4 @@
+﻿# Islamic Quiz App
+ To test your basic Islamic Knowledge, Answer the Quiz.
+ See your score at the end of the Quiz.
+ Highlights the correct and the wrong selected options.
---
+++ Tic-Tac-Toe game/React-Projects/Islamic Quiz App/src/assets/data.js
@@ -0,0 +1,84 @@
+ export const data = [
+    {
+      question: "What is the holy book of Islam?",
+        option1: "Torah",
+        option2: "Quran",
+        option3: "Vedas",
+        option4: "Bible",
+        ans: 2
+    },
+    {
+      question: "Who is considered the final prophet in Islam?",
+        option1: "Jesus",
+        option2: "Moses",
+        option3: "Abraham",
+        option4: "Muhammad",
+        ans: 4
+    },
+    {
+      question: "What is the term for the declaration of faith in Islam?",
+        option1: "Salat",
+        option2: "Jakaat",
+        option3: "Shahada",
+        option4: "Sawm",
+        ans: 3
+    },
+    {
+      question: "What is the pilgrimage to Mecca called?",
+        option1: "Hajj",
+        option2: "Umrah",
+        option3: "Ramadan",
+        option4: "Eid",
+        ans: 1
+    },
+    {
+      question: "What is the name of the month of fasting observed by Muslims?",
+        option1: "Shawwal",
+        option2: "Dhul-Hijjah",
+        option3: "Ramadan",
+        option4: "Eid",
+        ans: 3
+    },
+    {
+      question: "What is the Arabic term for charitable giving in Islam?",
+      option1: "Salat",
+      option2: "Sawm",
+      option3: "Zakaat",
+      option4: "Hajj",
+      ans: 3
+    },
+    {
+      question: "What is the direction Muslims face when praying?",
+      option1: "North",
+      option2: "South",
+      option3: "East",
+      option4: "West",
+      ans: 3
+    },
+    {
+      question: "What is the name of the holy city in Islam where the Prophet Muhammad was born?",
+      option1: "Damascus",
+      option2: "Medina",
+      option3: "Jerusalem",
+      option4: "Mecca",
+      ans: 4
+    },
+    {
+      question: "What are the five pillars of Islam?",
+      option1: "Faith, Charity, Prayer, Fasting, Pilgrimage",
+      option2: "Prayer, Fasting, Charity, Faith, Recitation",
+      option3: "Fasting, Charity, Pilgrimage, Faith, Prayer",
+      option4: "Shahada, Salah, Zakat, Sawm, Hajj",
+      ans: 4
+    },
+    {
+      question: "What is the term for the Islamic law derived from the Quran and Hadith?",
+      option1: "Sharia",
+      option2: "Fiqh",
+      option3: "Sunnah",
+      option4: "Halal",
+      ans: 1
+    }
+  ];
+  
+  
